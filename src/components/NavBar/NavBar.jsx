2e1fd93496d7a1e312bb29b0d882368776ab4b11
Alvaro Secuoyas <alvaro.sanchez@secuoyas.com>
2024-04-11 18:21:07 +0200

    primer commit

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <div className="nav">
+      <ul>
+        <li>AVENTURAS</li>
+        <li>PERSONAJES</li>
+        <li>MAPAS</li>
+      </ul>
+    </div>
+  )
+}
+
+export default NavBar
