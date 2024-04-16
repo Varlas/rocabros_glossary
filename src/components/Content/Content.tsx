3e6007d26e80e415df01a2b8803cf6e3a24dcf4f
Alvaro Secuoyas <alvaro.sanchez@secuoyas.com>
2024-04-16 21:09:42 +0200

    Content styles added

---
+++ src/components/Content/Content.tsx
@@ -0,0 +1,18 @@
+import './Content.css';
+import DefaultTemplate from '../DefaultTemplate/DefaultTemplate';
+
+const Content = ({ selectedId, list: listBulk }) => {
+  const list = !!listBulk ? listBulk : null;
+
+  const contentToShow =
+    !!list && selectedId
+      ? list.filter((item) => item.id === selectedId)[0].description
+      : null;
+  return selectedId && contentToShow ? (
+    <div className="main-content">{contentToShow}</div>
+  ) : (
+    <div className="main-content">{contentToShow}</div>
+  );
+};
+
+export default Content;
